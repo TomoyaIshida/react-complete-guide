99baea07a956298bbf8503b63c2c4faa9d1cff2b
TomoyaIshida <tmyisd@icloud.com>
2018-10-21 23:33:51 +0900

    import withclass

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,9 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
+
 
 class App extends PureComponent {
   constructor(props) {
@@ -91,7 +94,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -99,9 +102,9 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-      </div>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
