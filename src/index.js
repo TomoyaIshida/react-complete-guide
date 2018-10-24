a80811c23a0f05b7a052fa4a0237cc4e62eb6a24
TomoyaIshida <tmyisd@icloud.com>
2018-10-24 16:41:15 +0900

    delete old file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App title="Relevant Persons"/>, document.getElementById('root'));
-serviceWorker.unregister();
