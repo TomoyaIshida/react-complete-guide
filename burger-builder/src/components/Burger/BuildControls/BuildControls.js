70fda57e4ec70dd4be6947e3d54f5d091b93f60f
TomoyaIshida <tmyisd@icloud.com>
2018-10-29 14:11:36 +0900

    Implement removeIngredientHandler

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -16,9 +16,25 @@ const buildControls = (props) => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
-        added={() => props.ingredientAdded(ctrl.type)} />
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]} />
     ))}
   </div>
 );
+
+// const buildControls = (props) => (
+//   <div className={classes.BuildControls}>
+//     {controls.map(ctrl => (
+//       <BuildControl
+//         key={ctrl.label}
+//         label={ctrl.label}
+//         added={() => props.ingredientAdded(ctrl.type)}
+//         removed={() => props.ingredientRemoved(ctrl.type)}
+//         disabled="true" />
+//     ))}
+//   </div>
+// );
+
 
 export default buildControls;
