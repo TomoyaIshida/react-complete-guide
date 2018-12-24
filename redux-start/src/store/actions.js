b7d71a62c0e93fa47b08f97fb06032e0ac1d754d
TomoyaIshida <tmyisd@icloud.com>
2018-12-25 00:42:22 +0900

    Implement action creators

--- redux-start/src/store/actions.js
+++
@@ -1,6 +0,0 @@
-export const INCREMENT = 'INCREMENAT';
-export const DECREMENT = 'DECREMENAT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE_RESULT = 'STORE_RESULT';
-export const DELETE_RESULT = 'DELETE_RESULT';
