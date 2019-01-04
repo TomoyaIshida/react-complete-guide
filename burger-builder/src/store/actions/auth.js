b6091c276efe71ea1518db62ec24a90bfd3c7fc3
TomoyaIshida <tmyisd@icloud.com>
2019-01-04 12:51:54 +0900

    Implement logout function

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
 	};
 };
 
+export const logout = () => {
+	return {
+		type: actionTypes.AUTH_LOGOUT
+	};
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(logout());
+		}, expirationTime * 1000)
+	};
+};
+
+
 export const auth = (email, password, isSignup) => {
 	return dispatch => {
 		dispatch(authStart());
@@ -39,9 +54,9 @@ export const auth = (email, password, isSignup) => {
 			.then(response => {
 				console.log(response);
 				dispatch(authSuccess(response.data.idToken, response.data.localId));
+				// dispatch(checkAuthTimeout(err.response.data.error))
 			})
 			.catch(err => {
-				console.log(err);
 				dispatch(authFail(err.response.data.error));
 			})
 	};
